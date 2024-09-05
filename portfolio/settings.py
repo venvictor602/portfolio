3ae54a4beb8835f9b2848613490403e015e4bd3f
venvictor602 <venvictor602@gmail.com>
2024-09-05 12:24:52 +0100

    static

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q#0&3486(ntk%+^50!!8n*15u_2##q%q$=vd3e)=-ju7z^&f2o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
