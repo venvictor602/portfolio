ec81981eb236c6712a6543629fe4ad191ae4671d
venvictor602 <venvictor602@gmail.com>
2024-09-05 10:32:13 +0100

    final

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,7 @@
-# build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+#!/bin/bash
+# Install dependencies using the correct Python environment
+python3 -m pip install --upgrade pip  # Upgrade pip to ensure compatibility
+python3 -m pip install -r requirements.txt  # Install required dependencies
+
+# Run Django collectstatic to collect static files into STATIC_ROOT
+python3 manage.py collectstatic --noinput
