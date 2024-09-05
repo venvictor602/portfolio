6812bcac6fe6a72d6d150d2d08ba18cc61c20ba6
venvictor602 <venvictor602@gmail.com>
2024-09-05 11:10:26 +0100

    handler

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -7,10 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
+
+# application = get_wsgi_application()
+
+import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
 application = get_wsgi_application()
+
+# Vercel expects 'handler' or 'app', so alias 'application' as 'handler'
+handler = application
