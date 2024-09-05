683335ce009c478d469c3dfe85978e7398126882
venvictor602 <venvictor602@gmail.com>
2024-09-05 12:17:58 +0100

    static

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -129,11 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 import os
-# STATIC_ROOT = BASE_DIR / 'productionfiles'
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'mystaticfiles'
-# ]
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'productionfiles')
 
